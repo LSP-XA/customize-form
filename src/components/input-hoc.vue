6fa79cf14082ad7ae5a60c1d5c1d205a4e382fa3
spark <jdz321@qq.com>
2021-01-19 11:57:05 +0800

    feat: support slots in HOC

--- src/components/input-hoc.vue
+++ src/components/input-hoc.vue
@@ -1,3 +1,14 @@
-<template>
-  <el-input v-on="$listeners" v-bind="$attrs" />
-</template>
+<script>
+import ElInput from 'element-ui/packages/input';
+
+export default {
+  name: `Wrapped${ElInput.name}`,
+  render(h) {
+    return h(ElInput, {
+      on: this.$listeners,
+      attrs: this.$attrs,
+      scopedSlots: this.$scopedSlots,
+    });
+  },
+};
+</script>
